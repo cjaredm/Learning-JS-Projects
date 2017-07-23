ec49a4193fae210919f566419fd0bd4e67607fef
cjaredm <cjared.mortenson@gmail.com>
2017-07-23 10:48:35 -0700

    Slow to Update

--- Practice Portfolio/scripts/scripts.js
+++ Practice Portfolio/scripts/scripts.js
@@ -1,4 +1,8 @@
-//Flex Panels Open and close
+//import {something} from './src/components/queryDOM';
+//import { konami } from './src/components/Component';
+
+
+/**************Flex Panels Open and close****************/
 const panels = document.querySelectorAll('.panel');
 
 //Toggle Open a new panel
@@ -24,13 +28,37 @@ panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 
 
-//Konami Code Fun thing
+
+
+/* Sticky Nav */
+const nav = document.querySelector('.nav');
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll', fixNav);
+
+
+
+
+
+
+
+
+/*****************Konami Code Fun thing*************/
 const pressed = [];
 const konami = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 window.addEventListener('keyup', (e) => {
     pressed.push(e.key);
     pressed.splice(-konami.length - 1, pressed.length - konami.length);
-    console.log(pressed);
 
     if (pressed.join('') == konami.join('')) {
         console.log('Komani Code entered successfully!');
@@ -38,41 +66,95 @@ window.addEventListener('keyup', (e) => {
         //Add Something in here that would be fun.
         window.location.href = "https://en.wikipedia.org/wiki/Konami_Code";
     }
-
 });
 
+
+
+
+
+
+
+
 // NEXT SECTION
-// Radial Animation //http://jsfiddle.net/loktar/uhVj6/4/
+/*************Radial Animations*****************/
+//http://jsfiddle.net/loktar/uhVj6/4/
 
-const canvas = document.querySelector('#radialOne');
-const context = canvas.getContext('2d');
+let currentPercent = 1; //Compared to endPercent so it knows to end.
+function animate(rads, current) {
+    const context = rads.getContext('2d');
+    //Starting coordinates
+    const x = rads.width / 2; //middle of canvas
+    const y = rads.height / 2; //middle of canvas
+    const radius = 0.38 * rads.width; //Radius of circle in pixels
+    const endNum = rads.getAttribute('data-num');
+    const endPercent = +rads.getAttribute('data-num') + +1; //Ending % of circle
+    const fullCircle = Math.PI * 2; //= 360 degrees in radians
+    const quarterClock = Math.PI / 2; //This equals 25% of a circle used later to move start point from 3 o'clock to 12.
 
-//Starting coordinates
-const x = canvas.width / 2; //middle of canvas
-const y = canvas.height / 2; //middle of canvas
-const radius = 90; //Radius of circle in pixels
-const endPercent = 77; //Ending % of circle
-let curPerc = 0; //Compared to endPercent so it knows to end.
-const fullCircle = Math.PI * 2; //= 360 degrees in radians
-const quarterClock = Math.PI / 2; //This equals 25% of a circle used later to move start point from 3 o'clock to 12.
+    context.lineWidth = 10; // Line width
+    context.strokeStyle = rads.getAttribute('data-color'); //Line Color
 
-context.lineWidth = 10; // Line width
-context.strokeStyle = '#ad2323'; //Line Color
-
-
-function animate(current) {
-    context.clearRect(0, 0, canvas.width, canvas.height); //Clear Canvas to empty
     context.beginPath();
     //https://www.w3schools.com/tags/canvas_arc.asp
     context.arc(x, y, radius, -(quarterClock), ((fullCircle) * current) - quarterClock, false);
     context.stroke(); //Draw the line
-    curPerc++; // +1%
+    currentPercent++; // +1%
 
-    if (curPerc < endPercent) { //If the +1 didn't put it to the endPercent then do it again, starting at the current percentage
-        requestAnimationFrame(() => {
-            animate(curPerc / 100)
-        });
+    //Canvas Text
+    context.font = "bold " + (radius * 0.7) + "px serif";
+    context.textBaseline = "top";
+    context.textAlign = "center";
+    context.fillText(endNum, x /*- (x * 0.25)*/ , y - (y * 0.3));
+
+    if (currentPercent < endPercent) { //If the +1 didn't put it to the endPercent then do it again, starting at the current percentage
+        requestAnimationFrame(() => animate(rads, (currentPercent / 100)));
     }
+    context.closePath();
 }
 
-animate();
+
+/* Animate the radials that come next when scrolled through */
+//This will have it run only every so often instead of every millisecond
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const radials = document.querySelectorAll(".radials");
+
+function onScreenYet(e) {
+    radials.forEach(rad => {
+        const radOnScreen = (window.scrollY + window.innerHeight) - rad.height;
+        const radBottom = rad.offsetTop + rad.height;
+
+        const isOnScreen = radOnScreen > rad.offsetTop;
+        const isNotScrolledPassed = window.scrollY < radBottom;
+
+        if (isOnScreen && isNotScrolledPassed) {
+            rad.classList.add('animate');
+        } else {
+            rad.classList.remove('animate');
+        }
+    });
+}
+window.addEventListener('scroll', debounce(onScreenYet));
+
+
+
+//leave this here.
+radials.forEach(radial => {
+    const id = radial.getAttribute('id');
+    const radialCircle = document.querySelector(`#${id}`);
+    animate.call(radialCircle, radialCircle);
+});
